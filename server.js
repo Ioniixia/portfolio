6055dae8f457134fd2064736cf8f9cd10bad9496
Ioniixia <24aima41@kristujayanti.com>
2026-03-26 20:57:25 +0530

    fixed index.html path

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/api/contact', async (req, res) => {
 
 // Serve frontend for all other routes (Express 5 wildcard syntax)
 app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
